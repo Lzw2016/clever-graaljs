7f02f5292d502d96be6387ea2235f3eadff27eab
lizw <1183409807@qq.com>
2021-06-11 15:39:29 +0800

    内置对象实现 75%

---
+++ clever-graaljs-core/src/test/resources/builtin/CryptoUtilsTest.js
@@ -0,0 +1,12 @@
+const log = LoggerFactory.getLogger("CryptoUtilsTest.js");
+
+const input = StringUtils.getByteFromString("李志伟123");
+const key = CryptoUtils.generateAesKey();
+const iv = CryptoUtils.generateIV();
+// 加密
+const data = CryptoUtils.aesEncrypt(input, key, iv);
+const dataStr = EncodeDecodeUtils.encodeHex(data);
+log.info("加密 ---> {}", dataStr);
+// 解密
+const str = CryptoUtils.aesDecrypt(EncodeDecodeUtils.decodeHex(dataStr), key, iv);
+log.info("解密 ---> {}", str);
