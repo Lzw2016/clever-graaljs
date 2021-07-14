3fe1e86e01950874d0347887cb32788e842303a6
lizw <1183409807@qq.com>
2021-07-14 22:04:33 +0800

    clever-graaljs-fast-api 完成 81%

--- clever-graaljs-fast-api/src/main/java/org/clever/graaljs/fast/api/controller/GlobalController.java
+++ clever-graaljs-fast-api/src/main/java/org/clever/graaljs/fast/api/controller/GlobalController.java
@@ -4,10 +4,9 @@ import org.clever.graaljs.core.ScriptEngineInstance;
 import org.clever.graaljs.core.ScriptEngineInstanceStatus;
 import org.clever.graaljs.fast.api.config.FastApiConfig;
 import org.clever.graaljs.fast.api.model.FastApiGlobalEnv;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -16,7 +15,7 @@ import javax.annotation.Resource;
  * 创建时间：2021/07/05 13:37 <br/>
  */
 @RequestMapping("/fast_api")
-@Controller
+@RestController
 public class GlobalController {
     private static final String VERSION = "0.0.1-SNAPSHOT";
 
@@ -25,13 +24,6 @@ public class GlobalController {
     @Resource
     private ScriptEngineInstance scriptEngineInstance;
 
-    @GetMapping
-    public String indexPage() {
-        // noinspection SpringMVCViewInspection
-        return "/webjars/fast-api/index.html";
-    }
-
-    @ResponseBody
     @GetMapping("/global_env")
     public FastApiGlobalEnv getGlobalEnv() {
         FastApiGlobalEnv env = new FastApiGlobalEnv();
@@ -41,7 +33,6 @@ public class GlobalController {
         return env;
     }
 
-    @ResponseBody
     @GetMapping("/script_engine_status")
     public ScriptEngineInstanceStatus getStatus() {
         return scriptEngineInstance.getStatus();
--- clever-graaljs-fast-api/src/main/java/org/clever/graaljs/fast/api/controller/IndexPageController.java
+++ clever-graaljs-fast-api/src/main/java/org/clever/graaljs/fast/api/controller/IndexPageController.java
@@ -1,18 +1,16 @@
-package org.clever.graaljs.fast.api.demo.controller;
+package org.clever.graaljs.fast.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 作者：lizw <br/>
  * 创建时间：2021/07/13 21:28 <br/>
  */
-@RequestMapping("/")
 @Controller
 public class IndexPageController {
 
-    @GetMapping
+    @GetMapping("/fast-api.html")
     public String indexPage() {
         // noinspection SpringMVCViewInspection
         return "/webjars/fast-api/index.html";
