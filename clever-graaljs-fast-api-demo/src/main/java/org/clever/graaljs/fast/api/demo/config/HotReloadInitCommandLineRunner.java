900d64f719825fe0efd017e8913a401da5c1eccd
lizw <1183409807@qq.com>
2021-08-28 14:24:48 +0800

    clever-graaljs-fast-api 完成 91%

--- clever-graaljs-fast-api-demo/src/main/java/org/clever/graaljs/fast/api/demo/config/HotReloadInitCommandLineRunner.java
+++ clever-graaljs-fast-api-demo/src/main/java/org/clever/graaljs/fast/api/demo/config/HotReloadInitCommandLineRunner.java
@@ -1,16 +1,23 @@
 package org.clever.graaljs.fast.api.demo.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.clever.graaljs.core.utils.ExceptionUtils;
+import org.clever.graaljs.fast.api.config.FastApiConfig;
 import org.clever.graaljs.spring.mvc.DefaultExceptionResolver;
+import org.clever.graaljs.spring.mvc.ScriptHandlerCorsConfig;
 import org.clever.graaljs.spring.mvc.builtin.adapter.HttpContext;
 import org.clever.hot.reload.spring.HotReloadExtendUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.web.cors.CorsProcessor;
+import org.springframework.web.cors.DefaultCorsProcessor;
 
+import java.io.IOException;
 import java.util.Objects;
 
 /**
@@ -18,12 +25,15 @@ import java.util.Objects;
  * 创建时间：2021/08/28 13:03 <br/>
  */
 @AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
+@EnableConfigurationProperties({FastApiConfig.class})
 @Configuration
 @Slf4j
 public class HotReloadInitCommandLineRunner implements CommandLineRunner {
+    private final FastApiConfig fastApiConfig;
     private final ObjectProvider<ConversionService> conversionService;
 
-    public HotReloadInitCommandLineRunner(ObjectProvider<ConversionService> conversionService) {
+    public HotReloadInitCommandLineRunner(FastApiConfig fastApiConfig, ObjectProvider<ConversionService> conversionService) {
+        this.fastApiConfig = fastApiConfig;
         this.conversionService = conversionService;
     }
 
@@ -43,6 +53,15 @@ public class HotReloadInitCommandLineRunner implements CommandLineRunner {
                 }
             }
             return methodArgs;
+        };
+        final CorsProcessor corsProcessor = new DefaultCorsProcessor();
+        final ScriptHandlerCorsConfig corsConfig = fastApiConfig.getMvc().getCors() == null ? new ScriptHandlerCorsConfig() : fastApiConfig.getMvc().getCors();
+        HotReloadExtendUtils.CORS_PROCESSOR = (request, response) -> {
+            try {
+                return corsProcessor.processRequest(corsConfig.getCorsConfiguration(), request, response);
+            } catch (IOException e) {
+                throw ExceptionUtils.unchecked(e);
+            }
         };
     }
 }
